f205ad04695798ab659585c9569afde424291406
1408143 <1408143@rgu.ac.uk>
2017-02-28 14:14:51 +0000

    Push

---
+++ helloPrinter.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1408143
+ * Date: 28/02/2017
+ * Time: 14:01
+ */
+
+$query =$_GET['query'];
+
+echo $query;
---
+++ helloSelector.php
@@ -0,0 +1,14 @@
+<?php
+$myage=18;
+?>
+
+<!DOCTYPE  html>
+<html >
+<head>
+</head>
+<body>
+<p><a href="http://phpintro.azurewebsites.net/helloPrinter.php?query=Earth">Earth</a></p>
+<p><a href="http://phpintro.azurewebsites.net/helloPrinter.php?query=Mars">Mars</a></p>
+<p><a href="http://phpintro.azurewebsites.net/helloPrinter.php?query=Uranus">Uranus</a></p>
+</p></body>
+</html >
