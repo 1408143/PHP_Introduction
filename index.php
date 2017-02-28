6bf38e10d3f08282b585caea7b8221268ba7ec0e
1408143 <1408143@rgu.ac.uk>
2017-02-28 13:48:27 +0000

    Push

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+$myage=21;
+?>
+
 <!DOCTYPE  html>
 <html >
     <head>
@@ -5,7 +9,19 @@
     <body>
     <p>
         <?php
-        echo  " Hello  World";
+            if($myage >= 21){
+                echo "You can buy specs, mugs and sausage rolls.";
+            }else {
+                if ($myage >= 18) {
+                    echo "You can buy specs and mugs, but not sausage rolls.";
+                }else{
+                    if ($myage >= 16) {
+                        echo "You can buy specs, but not mugs and sausage rolls.";
+                    }else{
+                        echo "You can't buy anything.";
+                    }
+                }
+            }
         ?>
     </p>
     </body>
