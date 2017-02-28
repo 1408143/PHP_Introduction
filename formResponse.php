29a00951a1336336616d4b5f10bdad0bf0a97155
1408143 <1408143@rgu.ac.uk>
2017-02-28 14:31:36 +0000

    Push

---
+++ formResponse.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1408143
+ * Date: 28/02/2017
+ * Time: 14:24
+ */
+
+$firstname=$_POST['firstname'];
+$lastname=$_POST['lastname'];
+$gender=$_POST['gender'];
+$power=$_POST['power'];
+
+echo $firstname." ".$lastname." ".$gender." ".$power;
+?>
--- helloSelector.php
+++ helloSelector.php
@@ -10,5 +10,13 @@ $myage=18;
 <p><a href="http://phpintro.azurewebsites.net/helloPrinter.php?query=Earth">Earth</a></p>
 <p><a href="http://phpintro.azurewebsites.net/helloPrinter.php?query=Mars">Mars</a></p>
 <p><a href="http://phpintro.azurewebsites.net/helloPrinter.php?query=Uranus">Uranus</a></p>
-</p></body>
+
+    <form action="formResponse.php" method="post">
+        <label>First Name</label><input type="text" name="firstname">
+        <label>Last Name</label><input type="text" name="lastname">
+        <label>Gender</label><input type="text" name="gender">
+        <label>Main Power</label><input type="text" name="power">
+        <input type="submit" value="Submit">
+    </form>
+</body>
 </html >
