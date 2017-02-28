0e8dd72677df693f8672b808de2d976216da5724
1408143 <1408143@rgu.ac.uk>
2017-02-28 15:54:25 +0000

    Push

--- formLogin.php
+++ formLogin.php
@@ -9,12 +9,13 @@ $username="bob";
 $password="pass";
 
 $received_username=$_POST['username'];
-//echo $received_username;
+echo $received_username;
 $received_password=$_POST['password'];
-//echo $received_password;
+echo $received_password;
 if($username==$received_username && $password==$received_password){
     setcookie("access_level","standard_user",0);
     //echo $_COOKIE["access_level"];
-
     header("homePage.php");
+}else{
+    echo "nope";
 }
